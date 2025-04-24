db93e5ee3c7f1236e995111ff26c6538cc1e0d5f
Rakoski <mastrakoski@gmail.com>
2025-04-23 22:28:04 -0300

    first commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+                &copy; todos os direitos reservados
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
