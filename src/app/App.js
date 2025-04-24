420148a5ee23793c3e38d257fe3bce7a39a783b0
Rakoski <mastrakoski@gmail.com>
2025-04-23 22:38:40 -0300

    fix: updated imports

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './components/footer/footer';
-import Header from './components/header/header';
-import Calculadora from './components/calculadora/calculadora';
-import Home from './app/(unauthenticated)/home/page';
+import Footer from '../components/footer/footer';
+import Header from '../components/header/header';
+import Calculadora from '../components/calculadora/calculadora';
+import Home from './(unauthenticated)/home/page';
 
 function App() {
   return (
