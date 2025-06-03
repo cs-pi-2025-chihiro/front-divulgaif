a97cca53bb636b81c61a31844ef6361eda9d9682
Rakoski <mastrakoski@gmail.com>
2025-06-03 06:29:28 -0300

    feat: adicionado constants.js para testes em dev

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.BASE_URL || "http://localhost:8080";
+const isDev = process.env.NODE_ENV === "development";
+export const BASE_URL = isDev
+  ? "http://localhost:8080"
+  : "https://desenvolvimento.divulgaif.com.br";
