b915a69378830a76def77030d95d1cbe9839d1bc
Rakoski <mastrakoski@gmail.com>
2025-06-19 14:22:48 -0300

    fix: correção variáveis de ambiente

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
-const isDev = process.env.NODE_ENV === "development";
-export const BASE_URL = isDev
-  ? "http://localhost:8081/api/v1"
-  : "https://desenvolvimento.divulgaif.com.br/api/v1";
-
+const env = process.env.REACT_APP_ENV || process.env.NODE_ENV;
+export const BASE_URL = (() => {
+  switch (env) {
+    case "development":
+      return "http://localhost:8080";
+    case "production":
+      return "https://divulgaif.com.br";
+    default:
+      return "https://desenvolvimento.divulgaif.com.br";
+  }
+})();
 export const PAGE_SIZE = 8;
