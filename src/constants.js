a21ea28f0c889f1c65435cf22ceee2c0a7ef5f1b
Rakoski <mastrakoski@gmail.com>
2025-07-16 20:30:41 -0300

    feat: get by id de cada trabalho

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,14 @@
-const isDev = process.env.NODE_ENV === "development";
-export const BASE_URL = isDev
-  ? "http://localhost:8081/api/v1"
-  : "https://desenvolvimento.divulgaif.com.br/api/v1";
-
+const env = process.env.REACT_APP_ENV || process.env.NODE_ENV;
+export const BASE_URL = (() => {
+  switch (env) {
+    case "development":
+      return "http://localhost:8081/api/v1";
+    case "production":
+      return "https://divulgaif.com.br/api/v1";
+    case "staging":
+      return "https://desenvolvimento.divulgaif.com.br/api/v1";
+    default:
+      return "https://desenvolvimento.divulgaif.com.br/api/v1";
+  }
+})();
 export const PAGE_SIZE = 8;
--- src/enums/endpoints.js
+++ src/enums/endpoints.js
@@ -1,6 +1,7 @@
 export const ENDPOINTS = {
   WORKS: {
     LIST: "/works/list",
+    GET: "/works/",
   },
   SUAP: {
     OAUTH: "https://suap.ifpr.edu.br/o/authorize/",
