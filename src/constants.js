b9e52f398c4022710d69b7d7441d08e9645a222b
Rakoski <mastrakoski@gmail.com>
2025-07-21 07:01:14 -0300

    fix: correções menores

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const env = process.env.REACT_APP_ENV || process.env.NODE_ENV;
 export const BASE_URL = (() => {
   switch (env) {
     case "development":
-      return "https://desenvolvimento.divulgaif.com.br/api/v1";
+      return "http://localhost:8081/api/v1";
     case "production":
       return "https://divulgaif.com.br/api/v1";
     case "staging":
@@ -12,3 +12,5 @@ export const BASE_URL = (() => {
   }
 })();
 export const PAGE_SIZE = 8;
+export const aboutWebsite =
+  "https://chihiro-front.s3.sa-east-1.amazonaws.com/team-chihiro-front/sobre-produto-eduardo/pagina.html";
