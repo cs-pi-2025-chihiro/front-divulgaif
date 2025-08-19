67dc81a7b9799c1227e016d97ddc1e2462fd71d8
Rakoski <mastrakoski@gmail.com>
2025-08-19 08:04:25 -0300

    feat: página 404 ao entrar em rotas que o usuário precisa estar logado/não existem

---
+++ src/app/(unauthenticated)/404/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import Button from "../../../components/button";
+import "./page.css";
+
+const NotFound = () => {
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate(`/${i18n.language}`);
+  };
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <div className="error-code">404</div>
+        <h1 className="error-title">{t("notFound.title")}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
