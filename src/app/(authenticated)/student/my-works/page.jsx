b793969c3fd98f98b1d0a30182e6e94443897c34
arturflacon <flaconartur@gmail.com>
2025-09-26 20:25:58 -0300

    feat: loading correcoes

--- src/app/(authenticated)/student/my-works/page.jsx
+++ src/app/(authenticated)/student/my-works/page.jsx
@@ -18,7 +18,7 @@ import Loader from "../../../../components/loader";
 const MyWorks = () => {
   const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [appliedFilters, setAppliedFilters] = useState({});
-  const [currentPage, setCurrentPage] = useAtom(pageAto);
+  const [currentPage, setCurrentPage] = useAtom(pageAtom);
   const [currentSize, setCurrentSize] = useAtom(sizeAtom);
   const [search, setSearch] = useAtom(searchAtom);
   const { t, i18n } = useTranslation();
